dd3bf74397d54e5e6c69d73ac47e15f2d37af040
hl <hw1589998247@gmail.com>
2023-06-15 16:37:45 -0400

    create 3 empty components for tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Tab1Component } from './pages/tab1/tab1.component';
+import { Tab2Component } from './pages/tab2/tab2.component';
+import { Tab3Component } from './pages/tab3/tab3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Tab1Component,
+    Tab2Component,
+    Tab3Component
   ],
   imports: [
     BrowserModule,
