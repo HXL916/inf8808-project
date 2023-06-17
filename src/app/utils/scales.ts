29ea30ac14638339f6596ea67a625e1b8f29f6a6
Naxosia <clara.naxosia@gmail.com>
2023-06-17 12:38:21 -0400

    add :seat compo, data, utils + cleared app compo

---
+++ src/app/utils/scales.ts
@@ -0,0 +1,31 @@
+import * as d3 from 'd3';
+
+/**
+ * A UTILISER SUR LES CSV3
+ * @param {object} data The data to be displayed
+ * @returns {*} The ordinal scale used to determine the color
+ */
+export function setProvinceColorScale (data) {
+    // Set scale
+    // Get all continents from the data
+    let provinces = data.map(obj => obj.Province)
+    // Filter it to avoid duplicates and put in alphabetical order
+    provinces = provinces.filter((value, index, array) => array.indexOf(value) === index)
+    provinces.sort()
+    // Create a color scale for the provinces
+    var color = d3.scaleOrdinal().domain(provinces).range(d3.schemeTableau10)
+    return color
+  }
+
+
+/**
+ * A UTILISER SUR LE CSV2
+ * @param {object} data The data to be displayed
+ * @returns {*} The ordinal scale used to determine the color
+ */
+export function setPartyColorScale (data) {
+    let parties = data.map(obj => obj.Province)
+    // Create a color scale for the provinces
+    var color = d3.scaleOrdinal().domain(provinces).range(d3.schemeTableau10)
+    return color
+  }
