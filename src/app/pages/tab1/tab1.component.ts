067a2b549c92ef38932d3e7f77c19860cad016c7
hl <hw1589998247@gmail.com>
2023-06-19 17:51:52 -0400

    basic d3 setup for tab1

--- src/app/pages/tab1/tab1.component.ts
+++ src/app/pages/tab1/tab1.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: './tab1.component.html',
   styleUrls: ['./tab1.component.css']
 })
-export class Tab1Component implements OnInit {
+export class Tab1Component implements AfterViewInit  {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.createGraph();
   }
 
+  createGraph(): void {
+    // Get the graph container element
+    const container = d3.select('#graph-container');
+
+    // Define your graph logic using D3.js methods
+    // For example, create a simple SVG circle
+    container
+      .append('svg')
+      .attr('width', 200)
+      .attr('height', 200)
+      .append('circle')
+      .attr('cx', 100)
+      .attr('cy', 100)
+      .attr('r', 50)
+      .attr('fill', 'blue');
+  }
 }
