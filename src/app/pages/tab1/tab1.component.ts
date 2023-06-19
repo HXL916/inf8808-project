9794066ae4d22354d5f2fb5fdfca2305ecb5332a
hl <hw1589998247@gmail.com>
2023-06-19 18:36:46 -0400

    small changes

--- src/app/pages/tab1/tab1.component.ts
+++ src/app/pages/tab1/tab1.component.ts
@@ -8,14 +8,15 @@ import * as d3 from 'd3';
   styleUrls: ['./tab1.component.css']
 })
 export class Tab1Component implements AfterViewInit  {
+  itemList: any;
 
   constructor() {}
 
   ngAfterViewInit(): void {
-    this.createGraph(this.process("test"));
+    this.createGraph(this.process(this.itemList));
   }
 
-  createGraph(data:any): void {
+  createGraph(data:number): void {
     // Get the graph container element
     const container = d3.select('#graph-container');
 
@@ -31,7 +32,7 @@ export class Tab1Component implements AfterViewInit  {
       .attr('r', 50)
       .attr('fill', 'blue');
   }
-
+  // This function is just an example of how you can process your data
   process(data:any):number{
     console.log(data);
     return data;
