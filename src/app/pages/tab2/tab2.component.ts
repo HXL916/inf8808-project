260d699e7f239b3c513a9bc4d8fe6831fff86fc3
Naxosia <clara.naxosia@gmail.com>
2023-06-20 21:24:23 -0400

    affichage des carrés

--- src/app/pages/tab2/tab2.component.ts
+++ src/app/pages/tab2/tab2.component.ts
@@ -12,37 +12,59 @@ export class Tab2Component  implements AfterViewInit   {
   constructor() {}
 
   ngAfterViewInit(): void {
-    d3.json('./assets/data/deputesLegislatures.json').then( (data)=>{
-      console.log(data);
+    d3.csv('./assets/data/deputesLegislatures.csv', d3.autoType).then( (data)=>{
       this.createGraph(this.process(data));
     });
     
   }
 
-  createGraph(data:any): void {
+/**
+ * Draws the waffle chart
+ *
+ * @param {object[]} data The data to use
+ */
+  createGraph(data: { [key: string]: any }[]): void {
+    console.log(data);
     // Define geometry
-    let width,
-            height,
-            widthSquares = 16,
-            heightSquares = 9,
-            squareSize = 25,
-            squareValue = 0,
-            gap = 1;
-    width = (squareSize * widthSquares) + widthSquares * gap + 25;
-    height = (squareSize * heightSquares) + heightSquares * gap + 25;
+    let squareSize = 25,
+        smallGap = 12,
+        nbRowInBloc = 4,
+        nbColInBloc = 5,
+        nbBlocRow = 4,
+        nbBlocCol = 4,
+
+        nbSquaresInBloc = nbRowInBloc*nbColInBloc,  //20 squares per bloc
+        nbRow = nbRowInBloc*nbBlocRow,  // 16 rows of squares in total
+        nbCol = nbColInBloc*nbBlocCol,  // 20 columns of squares in total
+        nbSquares = nbRow*nbCol,  //320 squares for now
+
+        amountSquareBlocWidth = 5,
+        amountSquareBlocHeight = 4,
+        amountSquareBloc = amountSquareBlocHeight*amountSquareBlocWidth,  //20 squares per bloc
+
+        bigGap = 32,
+        alleyGap = 58,
+        
+        amountSquareGlobalWidth = amountSquareBloc*nbBlocCol,  //80 squares per line of bloc
+        amountSquareGlobalHeigth = amountSquareBloc*nbBlocRow,  
+
+        blocWidth = squareSize*amountSquareBlocWidth+smallGap*(amountSquareBlocWidth-1),  //173
+        blocHeight = squareSize*amountSquareBlocHeight+smallGap*(amountSquareBlocHeight-1), //136
+        width = blocWidth*nbBlocCol+bigGap*(nbBlocCol-1), //788 for now
+        height = blocHeight*nbBlocRow+bigGap*(nbBlocRow-2)+alleyGap;  //666 for now
 
     // Get color scale
     //let colorScale = setPartyColorScale(data);
 
-    // Get the graph container element
+    // Get the graph container element and create the svg
     const container = d3.select('#graph-container');
-
-    // Define your graph logic using D3.js methods
     const svg = container
       .append("svg")
       .attr('class', 'waffle')
       .attr("width", width)
       .attr("height", height);
+      //.attr('transform', 'translate(0,'+height+'+90+10)');  // 90 is the height of the banner
+
 
     svg.selectAll('rect')
       .data(data)
@@ -51,22 +73,39 @@ export class Tab2Component  implements AfterViewInit   {
       .attr("width", squareSize)
       .attr("height", squareSize)
       .attr("class", 'seat')
-      .attr("fill", 'black');//d => colorScale(d))
-      /*
-      .attr("x", function(d, i) {
+      .attr("fill", function(d){
+        return 'black';
+        //return colorScale(d.attribut);
+      }
+        )
+        .attr("x", function(d, i) {
           //group n squares for column
-          let col = Math.floor(i / heightSquares);
-          return (col * squareSize) + (col * gap);
+          let col = Math.floor(i / nbRow);
+          return (col * squareSize) + (col * smallGap);
       })
       .attr("y", function(d, i) {
-          let row = i % heightSquares;
-          return (heightSquares * squareSize) - ((row * squareSize) + (row * gap))
-      });*/
+          let row = i % nbRow;
+          return (nbRow * squareSize) - ((row * squareSize) + (row * smallGap))
+      });
+
+      // Improve placement of the squares
+      d3.selectAll("rect[x>='173']")
+        .attr('transform','translate(20,0)');
               
   }
 
-  // This function is just an example of how you can process your data
-  process(data:any):number{
-    return data;
+  /**
+ * Keeps only the MPs from the selected Legislature.
+ *
+ * @param {object[]} data The data to analyze
+ * @returns {object[]} output The data filtered
+ */
+  process(data: { [key: string]: any }[]):{ [key: string]: any }[]{
+
+    // Get the wanted Legislature from the slide button
+    var wantedLegislature = 44;
+
+    // Filter the MPs from this legislature
+    return data.filter((d)=>d['legislature'] == wantedLegislature);
   }
 }
