1d8a0f2f7f348932e1a1cc44ad7edf7ede5f47e3
Kekere <kerensh87@gmail.com>
2023-06-21 23:45:00 -0400

    Debut horizontal stacked bar chart

--- src/app/pages/tab1/tab1.component.ts
+++ src/app/pages/tab1/tab1.component.ts
@@ -14,6 +14,8 @@ import * as d3Legend from 'd3-svg-legend'
 export class Tab1Component implements AfterViewInit  {
   itemList!: any;
   color!: any;
+  xScale!: any;
+  yScale!: any;
 
   constructor(private leg:Legend) {}
 
@@ -26,15 +28,18 @@ export class Tab1Component implements AfterViewInit  {
       console.log(nbInterventionsByParty)
 
       let nbInterventionsByType:{ [key: string]: any }[] = preproc.getTypeInterventionCounts(data)
+      console.log(nbInterventionsByType)
 
-      this.createGraph(nbInterventionsByParty, parties)
+      this.createGraph(nbInterventionsByParty, nbInterventionsByType, parties)
+      this.yScale;
+      this.xScale;
 
       //drawLegend.drawLegend(partyColorScale, 400, parties)
       this.drawLegend(parties)
     })
   }
 
-  createGraph (data: { [key: string]: any }[], parties:string[]): void {
+  createGraph (data: { [key: string]: any }[], nbint: { [key: string]: any }[], parties:string[]): void {
     // Get the graph container element
     const container = d3.select('#waffleChart');
 
@@ -49,8 +54,50 @@ export class Tab1Component implements AfterViewInit  {
       .append('rect')
       .attr('class','tile')
 
+    const margin = { top: 10, right: 10, bottom: 20, left: 40 };
+
+    const width = 300 - margin.left - margin.right;
+    const height = 100 - margin.top - margin.bottom;
+    var total=nbint.reduce((total,arg)=>total+arg['Count'],0);
+    
+
+    const svgbar=d3.select('#stackedBarChart')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+    this.xScale = d3.scaleLinear()
+    .domain([0, d3.max(nbint, d => d['Count'])])
+    .range([0, width]);
+    console.log(d3.max(nbint, d => d['Count']))
+  
+    this.yScale = d3.scaleBand()
+      .domain(nbint.map(d => d['Parti']))
+      .range([0, height])
+      .padding(0.1);
+
+    // Create and append the bars
+    svgbar.selectAll(".bar")
+      .data(data)
+      .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", 0)
+      .attr("y", d => this.yScale(d['Count']))
+      .attr("width", d => this.xScale(d['Parti']))
+      .attr("height", this.yScale.bandwidth());
+
+    // Add x-axis
+    svgbar.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(this.xScale));
+
+    // Add y-axis
+    svgbar.append("g")
+      .call(d3.axisLeft(this.yScale));
 
   }
+
+  
 
   drawLegend(parties:string[]):void{
     console.log(parties)
