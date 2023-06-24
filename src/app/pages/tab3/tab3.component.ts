e2bfc94cd9be8dbee69cd9ba7d96a9aefc67d51e
Naxosia <clara.naxosia@gmail.com>
2023-06-24 18:01:46 -0400

    layout tab3

--- src/app/pages/tab3/tab3.component.ts
+++ src/app/pages/tab3/tab3.component.ts
@@ -8,32 +8,25 @@ import { PreprocessingService } from 'src/app/services/preprocessing.service';
   styleUrls: ['./tab3.component.css']
 })
 export class Tab3Component  implements AfterViewInit  {
+  wantedKey:string;
 
-  constructor(private preprocessingService: PreprocessingService) {}
+  constructor(private preprocessingService: PreprocessingService) {
+    this.wantedKey='genre';
+  }
 
   ngAfterViewInit(): void {
-    this.createGraph(this.process("test"));
+    
   }
 
   createGraph(data:any): void {
-    // Get the graph container element
-    const container = d3.select('#graph-container');
-
-    // Define your graph logic using D3.js methods
-    // For example, create a simple SVG circle
-    container
-      .append('svg')
-      .attr('width', 200)
-      .attr('height', 200)
-      .append('circle')
-      .attr('cx', 100)
-      .attr('cy', 100)
-      .attr('r', 50)
-      .attr('fill', 'blue');
+    
   }
-  // This function is just an example of how you can process your data
-  process(data:any):number{
-    console.log(data);
-    return data;
+
+  updateWantedKey(key:string):void{
+    this.wantedKey=key;
+    this.updateView();
+  }
+  updateView():void{         //importer data une fois seulment à place de le refaire à chaque changement
+    //this.createGraph(this.process(this.sortedData[this.wantedLegislature]));
   }
 }
