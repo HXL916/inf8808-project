34d5a318adfe294d0217103b11ede508a1d8baec
Naxosia <clara.naxosia@gmail.com>
2023-06-25 23:11:04 -0400

    fixed error on province legend

--- src/app/utils/scales.ts
+++ src/app/utils/scales.ts
@@ -11,7 +11,8 @@ export const genderColorScale = d3.scaleOrdinal() // D3 Version 4
 
 export const provinceColorScale = d3.scaleOrdinal() // D3 Version 4
 .domain(["Alberta", "Colombie-Britannique", "Manitoba", "Ontario", "Provinces maritimes", "Québec", "Saskatchewan", "Territoires"])
-.range(["#4E79A7","#F28E2C", "#59A14F", "#76B7B2", "#59A14F", "#EDC949", "#AF7AA1", "#FF9DA7"]);
+//.range(["#4E79A7","#F28E2C", "#59A14F", "#76B7B2", "#59A14F", "#EDC949", "#AF7AA1", "#FF9DA7"]);
+.range(d3.schemeTableau10);
 
 
 // export default { partyColorScale: partyColorScale } 
