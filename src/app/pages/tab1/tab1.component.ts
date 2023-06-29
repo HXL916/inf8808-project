1eae1ba5754da3e689163d1179de39af243e68e9
ehuard <ehuard.eh@gmail.com>
2023-06-28 21:34:56 -0400

    move stackedBar tab1 in tis own file + comments

--- src/app/pages/tab1/tab1.component.ts
+++ src/app/pages/tab1/tab1.component.ts
@@ -6,6 +6,7 @@ import * as d3Legend from 'd3-svg-legend'
 import * as waffle from 'src/app/pages/tab1/waffle';
 import { PreprocessingService } from 'src/app/services/preprocessing.service';
 import * as preproc from './preprocessTab1'
+import * as stackedBarChart from 'src/app/pages/tab1/stackedBar'
 
 
 @Component({
@@ -44,7 +45,7 @@ export class Tab1Component implements OnInit  {
       // Preprocess
       let popularInterventions:{ [key: string]: any }[] = this.preprocessingService.popularInterventions
       // Viz
-      this.createStackedBarChart(popularInterventions)
+      stackedBarChart.createStackedBarChart(popularInterventions)
 
 
       // get the interventions for the current legislature (44)
@@ -54,10 +55,9 @@ export class Tab1Component implements OnInit  {
       // KEY VALUES with deputesLegislatures.csv + TOP & FLOP
         const listeDeputes44:{ [key: string]: any }[] = this.preprocessingService.listeDeputes44
         // preprocessing for top & flop
-        let interestingMPs = this.preprocessingService.interestingMPs
+        let interestingMPs = this.preprocessingService.interestingMPs 
         this.topMPs = interestingMPs["topMPs"]
         this.flopMPs = interestingMPs["flopMPs"]
-        this.preprocessingService.getInterestingMPs(listeDeputes44, recentInterventions)
         // prepcoessing for Key value: increase in number of women
         this.addingStatIncreaseWomen()
         // KEY VALUE 1 : percentage of MP who spoke each month on average
@@ -87,88 +87,15 @@ export class Tab1Component implements OnInit  {
   }
 
 
+  /**
+   * Key stats functions
+   */
 
-    createStackedBarChart (popularinterventions: { [key: string]: any }[]): void {
-
-  
-      const margin = { top: 10, right:0, bottom: 10, left: 30 };
-  
-      const width = 1000 - margin.left - margin.right;
-      const height = 150 - margin.top - margin.bottom;
-  
-      const svg = d3.select('#stackedBarChart')
-          .attr('width', width + margin.left + margin.right)
-          .attr('height', height + margin.top + margin.bottom)
-          .append('g')
-          .attr('transform', `translate(${margin.left},${margin.top})`);
-
-
-      let xMax:number = 0;
-      for (const obj of popularinterventions) {
-        xMax += obj["Count"];
-      }
-      let cumulative_count:number = 0
-      popularinterventions.forEach((d) => {
-          d["Beginning"] = cumulative_count;
-          cumulative_count = cumulative_count + d["Count"]
-          d["End"] = cumulative_count;
-          d["Percentage"] = Math.round(d["Count"] / xMax * 1000)/10
-        });
-  
-      const types: string[] = popularinterventions.map((obj) => obj["TypeIntervention"]);
-      const x = d3.scaleLinear()
-          .domain([0, xMax])
-          .range([0, width]);
-  
-  
-      const colorScale: any = d3.scaleOrdinal()
-          .domain(types)
-          .range(d3.schemeTableau10);
-
-
-
-
-      const stack = svg.selectAll('.stack')
-        .data(popularinterventions)
-        .enter()
-        .append('g')
-        .attr('class', 'stack')
-        .attr('transform', (d) => `translate(${x(d["Beginning"])}, 0)`)
-
-      stack
-        .append('rect')
-        .attr('x', 0)
-        .attr('width', (d) => x(d[1]) - x(d[0]))
-        .attr('width', function(d) { return x(d["End"] - d["Beginning"])})
-        .attr('height', height)
-        .attr('fill', function(d) { return colorScale(d["TypeIntervention"])});
-
-      //Ajoute type d'interventions
-      stack
-        .append('text')
-        .attr('x', (d) => x(d["End"] - d["Beginning"])/2)
-        .attr('y', 25)
-        .attr('text-anchor', 'middle')
-        .attr("fill", "white")
-        .attr("font-size", "14px")
-        .attr("font-weight", "bold")
-        .text((d) => d["TypeIntervention"])
-        .call(wrap, 45);
-
-      //Ajoute pourcentage d'interventions
-      stack
-        .append('text')
-        .attr('x', (d) => x(d["End"] - d["Beginning"])/2)
-        .attr('y', height - 20)
-        .attr('text-anchor', 'middle')
-        .attr("fill", "white")
-        .attr("font-weight", "bold")
-        .text((d) => d["Percentage"]+"%");
-  }
-
-
+  // get the increase in women between the last 2 legislatures, and inject it in html code
   addingStatIncreaseWomen():void{
-    const increaseWomen:string = this.preprocessingService.increaseWomen
+    // get the increase in women already preprocessed in the service (it's static: we compute it only once)
+    const increaseWomen:string = this.preprocessingService.increaseWomen 
+    // get the html element where to put this stat, and then add this stat as text
     const statSpan3: HTMLSpanElement | null = document.getElementById("stat3") as HTMLSpanElement;
     if (statSpan3) {
       // Inject the value into the <span> element
@@ -176,36 +103,43 @@ export class Tab1Component implements OnInit  {
     }
   }
 
+  // get the average of the number of MPs who participated for each month, and inject it in html code
   addingStatActiveMPs(listeDeputes: { [key: string]: any }[], interventionData:{ [key: string]: any }[]):void{
+    // get the html element where to put this stat
     const statSpan1: HTMLSpanElement | null = document.getElementById("stat1") as HTMLSpanElement;
     if (statSpan1) {
+      // get the statistic already preprocessed in the service (it's static: we compute it only once)
       const percentActiveMPs:number = preproc.getPecentageActiveMP(listeDeputes, interventionData)
       // Inject the value into the <span> element
       statSpan1.textContent = percentActiveMPs.toString();
     }
   }
 
+  // get the number of MPs who changed their affiliation to a political party during the current legislature and inject it in html code
   addingStatChangeLegislature(changesLegislature44: { [key: string]: any }[]){
+    // changesLegislature44 = array with all the MP who changed their party during the current legislature
+    // it should be processed in the service because it's static (we use it only once)
     const statSpan2: HTMLSpanElement | null = document.getElementById("stat2") as HTMLSpanElement;
     if (statSpan2) {
+      // for question of style, we create an inner span. The text in this span will be bold and big (class = statValue)
+      // depending of the value of the stat, we also change the text in statSpan2
       const innerStatSpan: HTMLSpanElement = document.createElement("span");
       innerStatSpan.classList.add("statValue");
-      if(changesLegislature44.length == 0){
-        innerStatSpan.textContent = "Aucun"
-        const textAfter: Text = document.createTextNode(" député n'a")
+      if(changesLegislature44.length == 0){ // if we didn't notce any change
+        innerStatSpan.textContent = "Aucun" // this text will be bold
+        const textAfter: Text = document.createTextNode(" député n'a") // this text will be normal
         statSpan2.appendChild(innerStatSpan)
         statSpan2.appendChild(textAfter)
       }
-      else if(changesLegislature44.length == 1){
-        innerStatSpan.textContent = "1"
-        const textAfter: Text = document.createTextNode("députe ("+changesLegislature44[0]["nom"]+") a")
+      else if(changesLegislature44.length == 1){ // if only one MP changed his party, we diplay his name
+        innerStatSpan.textContent = "1" // this text will be bold
+        const textAfter: Text = document.createTextNode("députe ("+changesLegislature44[0]["nom"]+") a") // this text will be normal
         statSpan2.appendChild(innerStatSpan)
         statSpan2.appendChild(textAfter)
       }
-      else{
-        //statSpan2.textContent = changesLegislature44.length.toString()+" députés ont";
-        innerStatSpan.textContent = changesLegislature44.length.toString()
-        const textAfter: Text = document.createTextNode(" députés ont")
+      else{ // several changes
+        innerStatSpan.textContent = changesLegislature44.length.toString()  // this text will be bold (number of MPs who changed)
+        const textAfter: Text = document.createTextNode(" députés ont") // this text will be normal
         statSpan2.appendChild(innerStatSpan)
         statSpan2.appendChild(textAfter)
       }
@@ -215,50 +149,3 @@ export class Tab1Component implements OnInit  {
 
 
 
-function wrap(text: d3.Selection<SVGTextElement, { [key: string]: any }, SVGGElement, unknown>, width: number) {
-  text.each(function (this: SVGTextElement, d) {
-    const text = d3.select(this)
-    const words = text.text().split(/\s+/).reverse()
-    let word
-    let line:any = []
-    const lineHeight = 1.1 // Adjust this value for desired line height
-    const x = text.attr('x')
-    const y = text.attr('y')
-    const dy = parseFloat(text.attr('dy') || '0')
-    const maxLines = 4
-
-    let tspan = text
-      .text(null)
-      .append('tspan')
-      .attr('x', x)
-      .attr('y', y)
-      .attr('dy', dy + 'em')
-
-      let lineCount = 0;
-
-      while ((word = words.pop())) {
-        line.push(word);
-        tspan.text(line.join(' '));
-        if (tspan.node()!.getComputedTextLength() > width && line.length > 1) {
-          line.pop();
-          tspan.text(line.join(' '));
-          line = [word];
-          tspan = text
-            .append('tspan')
-            .attr('x', x)
-            .attr('y', y)
-            .attr('dy', ++lineCount * lineHeight + dy + 'em')
-            .text(word);
-        }
-      }
-  
-      // Check if the maximum number of lines is reached
-      if (lineCount >= maxLines) {
-        // Append "..." at the end of the last line
-        const lastTspan = text.selectAll('tspan').filter(':last-child');
-        const lastLineText = lastTspan.text();
-        const truncatedText = lastLineText.slice(0, lastLineText.length - 3) + '...';
-        lastTspan.text(truncatedText);
-      }
-  });
-}
