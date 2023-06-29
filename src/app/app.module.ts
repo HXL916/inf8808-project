0ee4655e468a555b5db8c7912d44ae12ceeb90e4
ehuard <ehuard.eh@gmail.com>
2023-06-28 22:14:50 -0400

    moved top flop component to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { Tab1Component } from './pages/tab1/tab1.component';
 import { Tab2Component } from './pages/tab2/tab2.component';
 import { Tab3Component } from './pages/tab3/tab3.component';
-import { TopFlopComponent } from './pages/tab1/top-flop/top-flop.component';
+import { TopFlopComponent } from './components/top-flop/top-flop.component';
 import { FilterButtonComponent } from './components/filter-button/filter-button.component';
 import { PreprocessingService } from './services/preprocessing.service';
 import { InterventionTypeToggleComponent } from './components/intervention-type-toggle/intervention-type-toggle.component';
