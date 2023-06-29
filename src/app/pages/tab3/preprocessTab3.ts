f149234ec08f20ddbb13b6fbbdf0f68e18d93783
lazureetienne <47014155+lazureetienne@users.noreply.github.com>
2023-06-29 00:49:13 -0400

    Added date to data inside each block

--- src/app/pages/tab3/preprocessTab3.ts
+++ src/app/pages/tab3/preprocessTab3.ts
@@ -100,7 +100,7 @@ export function getCountsWithKey(
 ): { [key: string]: any }[] {
   const tabCount: { [key: string]: number } = {};
   const tabCharCount: { [key: string]: number } = {};
-
+  const monthYear: { [key: string]: string } = {}; // Added monthYear object
   for (const obj of data) {
     if (obj.hasOwnProperty(wantedKey)) {
       const keyElement = obj[wantedKey];
@@ -111,12 +111,36 @@ export function getCountsWithKey(
         tabCount[keyElement] = 1;
         tabCharCount[keyElement] = obj['nbCaracteres'];
       }
+
+      // Add month and year to monthYear object
+      const month = obj['mois'];
+      const year = obj['année'];
+      const formattedDate = `${month}-${year}`;
+      if (!monthYear.hasOwnProperty(keyElement)) {
+        monthYear[keyElement] = formattedDate;
+      }
     }
   }
 
   // Define the specific order of keys
-  const specificOrder = 
-  ["H", "F", "PCC", "NPD", "PLC", "BQ", "PV", "Ind.", "Québec", "Ontario", "Colombie-Britannique", "Alberta", "Provinces maritimes", "Manitoba", "Saskatchewan", "Territoires"];
+  const specificOrder = [
+    "H",
+    "F",
+    "PCC",
+    "NPD",
+    "PLC",
+    "BQ",
+    "PV",
+    "Ind.",
+    "Québec",
+    "Ontario",
+    "Colombie-Britannique",
+    "Alberta",
+    "Provinces maritimes",
+    "Manitoba",
+    "Saskatchewan",
+    "Territoires"
+  ];
 
   // Here we order the tabCount object based on the specific order of keys
   const summarizedData: { [key: string]: any }[] = [];
@@ -125,7 +149,8 @@ export function getCountsWithKey(
       summarizedData.push({
         "KeyElement": element,
         "Count": tabCount[element],
-        "CharCount": tabCharCount[element]
+        "CharCount": tabCharCount[element],
+        "Date": monthYear[element] // Retrieve the date from monthYear object
       });
     }
   });
